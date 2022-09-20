e8b5856000afa6b1e95c98f82329548e3eeb3147
enriqueorea <enriqueorea28@gmail.com>
2022-09-20 14:34:54 -0500

    WIP: navbar component ui ready

---
+++ src/components/Navbar/SearchBar.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import { InputBase, Paper, IconButton } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import SearchIcon from "@mui/icons-material/Search";
+
+const SearchBar = () => {
+  return (
+    <Paper
+      component={"form"}
+      onSubmit={() => {}}
+      sx={{
+        borderRadius: 20,
+        border: "1px solid #e3e3e3",
+        pl: 2,
+        boxShadow: "none",
+        mr: { sm: 5 },
+      }}
+    >
+      <input
+        type="text"
+        className="search-bar"
+        placeholder="search ..."
+        value={""}
+        onChange={() => {}}
+      />
+      <IconButton type="submit" sx={{ p: "10px", color: "red" }}>
+        <SearchIcon />
+      </IconButton>
+    </Paper>
+  );
+};
+
+export default SearchBar;
