17f90ff57d417394d4156e7677d5eb135fb545a2
enriqueorea <enriqueorea28@gmail.com>
2022-09-20 19:47:18 -0500

    add video component

--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,5 @@ export { default as Feed } from "./Feed/Feed";
 export { default as ChannelDetail } from "./ChannelDetail/ChannelDetail";
 export { default as VideoDetail } from "./VideoDetail/VideoDetail";
 export { default as SearchFeed } from "./SearchFeed/SearchFeed";
+export { default as Sidebar } from "./Sidebar/Sidebar";
+export { default as Videos } from "./Videos/Videos";
